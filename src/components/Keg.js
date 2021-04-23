75874702afc23806c9775792a44c32a26eeabe55
Levi Kohler <kohler.la01@gmail.com>
2021-04-23 14:44:45 -0700

    update Keg with onClick function and new proptypes for id and whenKegCLicked

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 function Keg(props){
   return (
     <React.Fragment>
-      <h3>{props.name} - {props.brand}</h3>
-      <p>${props.price}</p>
-      <p><i>{props.alcoholContent}%</i></p>
-      <hr/>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+        <h3>{props.name} - {props.brand}</h3>
+        <p>${props.price}</p>
+        <p><i>{props.alcoholContent}%</i></p>
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -16,7 +18,9 @@ Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  alcoholContent: PropTypes.number.isRequired
+  alcoholContent: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
