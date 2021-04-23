0127796d20646a8fff8f31adbc43f57561fc4ac0
Levi Kohler <kohler.la01@gmail.com>
2021-04-23 11:09:42 -0700

    update NewKegForm to use ReusableForm component

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { v4 } from 'uuid';
 import PropTypes from "prop-types";
+import ResusableForm from "./ReusableForm";
 
 function NewKegForm(props) {
 
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: v4()});
+  }
+
+  return (
+    <React.Fragment>
+      <ResusableForm
+      formSubmissionHandler={handleNewKegFormSubmission}
+      buttonText="Add Keg" />
+    </React.Fragment>
+  );
 }
 
 NewTicketForm.propTypes = {
