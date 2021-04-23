17d5312ae60795627335b1d622004420f5efcb75
Levi Kohler <kohler.la01@gmail.com>
2021-04-23 10:53:58 -0700

    add NewKegForm skeleton

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+
+function NewKegForm(props) {
+
+}
+
+NewTicketForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
