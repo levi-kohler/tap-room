b8a2446495c3ea5959749cd23361e2c77ec16a98
Levi Kohler <kohler.la01@gmail.com>
2021-04-23 12:12:10 -0700

    update KegControl to allow changing states between KegList and AddNewKegForm

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import KegList from './KegList';
+import NewKegForm from './NewKegForm';
 
 class KegControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      formVisibleOnPage:false,
       masterKegList: []
     };
   }
 
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({masterKegList: newMasterKegList,
+                    formVisibleOnPage: false});
+  }
+
   render(){
     let currentlyVisibleState = null;
+    let buttonText = null;
+
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <KegList />
+      currentlyVisibleState = <NewKegForm onNewKegVreation={this.handleAddingNewKegToList} />;
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList kegList={this.state.masterKegList} onKegSelection={this.handleChangingSelectedKeg} />;
+      buttonText = "Add Keg";
     }
+
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>(buttonText</button>
       </React.Fragment>
     );
   }
