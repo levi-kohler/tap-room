6620bc033dace3c8b29706e8fe3600f432b3f9ee
Levi Kohler <kohler.la01@gmail.com>
2021-04-23 10:06:21 -0700

    add Keg component with skeleton code

---
+++ src/components/Keg.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props){
+  return (
+    <React.Fragment>
+
+    </React.Fragment>
+  );
+}
+
+Keg.PropTypes = {
+
+};
+
+export default Keg;
